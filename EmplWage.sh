e766dfa9493d5f39e5d3df2be4144c7c1375290f
RzT007clear <rajatburman7862gmail.com>
2020-10-11 17:57:12 +0530

    Solving use CASE statement

--- EmplWage.sh
+++ EmplWage.sh
@@ -3,16 +3,20 @@
 isFullTime=1
 isPartTime=2
 empRatePerHour=20
+
 randomCheck=$((RANDOM%3))
-if [ $randomCheck == $isFullTime ]
-then
-        empHrs=8
-elif [ $randomCheck == $isPartTime ]
-then
-        empHrs=4
-else
-        empHrs=0
-fi
+
+case $randomCheck in
+        $isFullTime )
+                empHrs=8 ;;
+        $isPartTime )
+                empHrs=4 ;;
+        * )
+                empHrs=0 ;;
+esac
+
         salary=$(($empRatePerHour*$empHrs))
 
 echo "Your one day salary is:" $salary
+
+
